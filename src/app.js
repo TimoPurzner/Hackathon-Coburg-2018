3791bd67e252000f1c7bf0853a4707268c613b6a
David <dev@dschwarz.codes>
2018-11-23 16:25:19 +0100

    [FIX] add stuff

--- src/app.js
+++ src/app.js
@@ -4,8 +4,9 @@ var alexa = require("alexa-app");
 var PORT = process.env.PORT || 8080;
 var app = express();
 
-// ALWAYS setup the alexa app and attach it to express before anything else.
 var alexaApp = new alexa.app("test");
+
+const debug = process.env.NODE_ENV !== 'production';
 
 alexaApp.express({
   expressApp: app,
@@ -17,13 +18,14 @@ alexaApp.express({
 
   // sets up a GET route when set to true. This is handy for testing in
   // development, but not recommended for production. disabled by default
-  debug: true
+  debug: debug
 });
 
 // now POST calls to /test in express will be handled by the app.request() function
 
 // from here on you can setup any other express routes or middlewares as normal
-app.set("view engine", "ejs");
+app.set("view engine", "pug");
+app.set('views', './src/views');
 
 alexaApp.launch(function(request, response) {
   response.say("You launched the app!");
