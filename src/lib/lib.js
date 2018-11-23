86be69fd3d196ab0e2b2d394b74705a2118303ec
Jonathan Braat <jonathan.braat@live.com>
2018-11-23 21:16:41 +0100

    [ADD] lib directory and lib.js with empty functions

---
+++ src/lib/lib.js
@@ -0,0 +1,26 @@
+export function getPrductionQueryString (... queryParams) {
+    return "there is nothing here yet";
+}
+
+export function getFilterOptions (query) {
+    return "there is nothing here yet";
+}
+
+export function getProductImageURL (imageId) {
+    return "there is nothing here yet";
+}
+
+export function getProductURL (masterSku, sku) {
+    return "there is nothing here yet";
+}
+
+/**
+ * (Method description)
+ * @param string: query
+ * @return string: URL
+ *
+ * returns URL of top product by search query
+ */
+export function getTopProductURL (query) {
+    return "there is nothing here yet";
+}
