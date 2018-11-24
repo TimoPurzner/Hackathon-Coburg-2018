dac0d1795210ba0209925dccc757bba8fec8e8eb
RACERXY <Alexander.Wollheim@stud.hs-coburg.de>
2018-11-24 11:45:14 +0100

    [ADD] getFilters + getFilterOptions

--- src/test.js
+++ src/test.js
@@ -1,5 +1,13 @@
 let customlib = require('./empiriecom/api.js');
 
-customlib.getTopProduct().then(bla => {
+// customlib.getTopProduct('iphone x').then(bla => {
+//     console.log(bla);
+// });
+
+customlib.getFilters('iphone x').then(bla => {
+    console.log(bla);
+});
+
+customlib.getFilterOptions('iphone x', 's_filter_Availability').then(bla => {
     console.log(bla);
 });
