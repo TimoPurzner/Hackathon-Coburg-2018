a4efb1957bdd2078f8ed617bdce426080e5516b5
David <dev@dschwarz.codes>
2018-11-24 10:11:42 +0100

    [FIX] change name

--- src/app.js
+++ src/app.js
@@ -168,7 +168,7 @@ alexaApp.intent("AMAZON.HelpIntent", function () {
 });
 
 if (process.env.NODE_ENV !== 'production') {
-    fs.writeFile('schema.json', alexaApp.schemas.askcli('go shopping'), (err) => {
+    fs.writeFile('schema.json', alexaApp.schemas.askcli('bauer shopping'), (err) => {
         if (err) throw err;
         console.log('Wrote schema.json');
     });
