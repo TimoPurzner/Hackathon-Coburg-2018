7347d4903b50ddaa1627d0698a2e2862d93f8f07
ZeroSoulEater <t1@purzner.de>
2018-11-24 15:42:26 +0100

    [FIX] filter for real

--- src/app.js
+++ src/app.js
@@ -171,8 +171,8 @@ alexaApp.intent("SelectFilterIntent", {
         let session = request.getSession();
 
         response.shouldEndSession(false);
-        console.log("SELECT FILTER INTENT", request.slots("FILTER_NAME").value);
-        response.say(request.slot("FILTER_NAME").value);
+        console.log("SELECT FILTER INTENT", request.slots["FILTER_NAME"].value);
+        response.say(request.slots["FILTER_NAME"].value);
 
     }
 );
